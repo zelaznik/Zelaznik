5136422011f7bc975bcb0a9b0a2147f5ef958c7d
Steve Zelaznik <zelaznik@yahoo.com>
2016-01-31 08:27:05 -0800

    The routes now point to the routes of the original templates.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
   resources :blog_posts
   root to: "pages#splash"
 
-  ['orig','home','about','blog','resume','portfolio','contact'].each do |choice|
+  ['home','about','blog','resume','portfolio','contact'].each do |choice|
     get choice, controller: "pages", action: choice
   end
 
   get 'next_splash', controller: "pages", action: 'splash'
+
+  get 'original.html', controller: "original_pages", action: "index"
+  get 'original/index.html', controller: "original_pages", action: "index"
+  get 'original/no-sidebar.html', controller: "original_pages", action: "no_sidebar"
+  get 'original/left-sidebar.html', controller: "original_pages", action: "left_sidebar"
+  get 'original/right-sidebar.html', controller: "original_pages", action: "right_sidebar"
 end
