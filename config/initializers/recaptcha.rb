36f1a15842f65ff537261c753134228488794cf2
Steve Zelaznik <zelaznik@yahoo.com>
2016-02-17 21:34:29 -0800

    Added environment recaptcha variables directly.

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 # config/initializers/recaptcha.rb
 Recaptcha.configure do |config|
-  config.public_key  = Rails.application.secrets.recaptcha_public_key
-  config.private_key = Rails.application.secrets.recaptcha_secret_key
+  config.public_key  = Rails.application.secrets.recaptcha_public_key || ENV['RECAPTCHA_PUBLIC_KEY']
+  config.private_key = Rails.application.secrets.recaptcha_secret_key || ENV['RECAPTCHA_SECRET_KEY']
   config.api_version = 'v2'
 end
