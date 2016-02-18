dfc243c1428a93f0878cab3b67c698b32511704f
Steve Zelaznik <zelaznik@yahoo.com>
2016-02-17 22:35:31 -0800

    Added public key to main site.

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,5 +2,4 @@
 Recaptcha.configure do |config|
   config.public_key  = Rails.application.secrets.recaptcha_public_key
   config.private_key = Rails.application.secrets.recaptcha_secret_key
-  config.api_version = 'v2'
 end
