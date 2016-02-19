43ab3a833964be549575591e59d68ba011fcef10
Steve Zelaznik <zelaznik@yahoo.com>
2016-02-18 20:37:20 -0800

    Forced recaptcha to always succeed.

--- app/helpers/recaptcha_helper.rb
+++ app/helpers/recaptcha_helper.rb
@@ -1,7 +1,16 @@
-require 'net/https'
+begin
+  require 'net/https'
+  e = nil
+rescue Exception => err
+  e = err
+end
 
 module RecaptchaHelper
   def verify_google_recaptcha
+    return {success: true}
+
+    raise e if e
+
     secret_key = Recaptcha.configuration.private_key
     response = params["g-recaptcha-response"]
     base_url = "https://www.google.com/recaptcha/api/siteverify"
